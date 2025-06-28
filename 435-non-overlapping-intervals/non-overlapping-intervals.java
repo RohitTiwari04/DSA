e7ef3f5671fe1886214a8395cac64764173e87ee
RohitTiwari04 <rohit.tiwari.cs2026@gmail.com>
2025-06-28 23:35:10 +0530

    Time: 50 ms (35.16%) | Memory: 73.5 MB (70.56%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -1,28 +1,59 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+
+        //Approch 1-->
+        // Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        // int n = intervals.length;
+        // int count = 0;
+        // int i = 0 , j = 1;
+
+        // while(j < n){
+        //     int currStart = intervals[i][0];
+        //     int currEnd = intervals[i][1];
+
+        //     int nextStart = intervals[j][0];
+        //     int nextEnd = intervals[j][1];
+
+        //     if(currEnd <= nextStart){
+        //         i = j;
+        //         j++;
+        //     }
+        //     else if(currEnd <= nextEnd){
+        //         j++;
+        //         count++;
+        //     }
+        //     else if(currEnd > nextEnd){
+        //         i = j;
+        //         j++;
+        //         count++;
+        //     }
+        // }
+
+        // return count;
+
+        //-----------------------------------------------------------------------------------
+        //Approch 2 ->
+
+        Arrays.sort(intervals , (a , b) -> Integer.compare(a[0],b[0]));
         int n = intervals.length;
         int count = 0;
-        int i = 0 , j = 1;
+        int i = 1;
+        int[] last_interval = intervals[0];
 
-        while(j < n){
-            int currStart = intervals[i][0];
-            int currEnd = intervals[i][1];
+        while(i < n){
 
-            int nextStart = intervals[j][0];
-            int nextEnd = intervals[j][1];
 
-            if(currEnd <= nextStart){
-                i = j;
-                j++;
+            if(last_interval[1] <= intervals[i][0]){
+                last_interval = intervals[i];
+                i++;
             }
-            else if(currEnd <= nextEnd){
-                j++;
+            else if(intervals[i][1] >= last_interval[1]){
+                i++;
                 count++;
             }
-            else if(currEnd > nextEnd){
-                i = j;
-                j++;
+            else if(intervals[i][1] < last_interval[1]){
+                last_interval = intervals[i];
+                i++;
                 count++;
             }
         }
