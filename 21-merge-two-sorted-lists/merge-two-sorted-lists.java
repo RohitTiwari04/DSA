0f1acadb7e99780817a7dc06d61c3d9d0cee0729
Rohit Tiwari <rohit.tiwari.cs2026@gmail.com>
2025-10-24 02:52:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (88.49%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -17,12 +17,12 @@ class Solution {
             if(list1.val <= list2.val){
                 curr.next = list1;
                 list1 = list1.next;
-                curr = curr.next;
+                
             }else{
                 curr.next = list2;
                 list2 = list2.next;
-                curr = curr.next;
             }
+            curr = curr.next;
         }
 
         if(list1 == null){
